52f366cafaf548e436da692bf1de873a37139768
Deepali4545 <deepalideheria1997@gmail.com>
2025-06-26 00:02:26 +0530

    all files

---
+++ src/Task/task03.java
@@ -0,0 +1,12 @@
+package Task;
+
+public class task03 {
+    public static void main(String[] args) {
+
+        System.out.println(10+1);
+        System.out.println(10-1);
+        System.out.println(10*1);
+        System.out.println(10%2);
+        System.out.println(10/2);
+    }
+}
